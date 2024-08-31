a681af7651bef6d475e6fbf6e8e912fd216781e3
SteveGongoraL <stv.brayton.gongora@gmail.com>
2024-08-31 00:33:43 -0600

    Se agrego la funcion anonima autoejecutada

---
+++ Ts-project/src/03-typing.ts
@@ -0,0 +1,9 @@
+// Funcion anonima autoejecutada: (() => {})();
+// Para poder usar las mismas variables en diferentes archivos
+
+(() => {
+  let myProductName = "Product 1";
+  let myProductPrice = 66;
+
+  myProductName.toLowerCase();
+})();
